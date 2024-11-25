6f4a8393519010baed3c65d65406a2e4d3928404
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-11-25 19:39:33 +0530

    hybrid model activated

--- frontend/src/store/atom.ts
+++ frontend/src/store/atom.ts
@@ -1,7 +1,8 @@
-import { roleType } from '@/interface/general'
+import { UserRole } from '@/interface/general'
 import { atom } from 'jotai'
 
 export const userAtom = atom(null)
-export const roleAtom = atom<roleType | null>(null)
+export const roleAtom = atom<UserRole | null>(null)
 export const postsAtom = atom([])
 export const settingsAtom = atom({ theme: 'light', language: 'en' })
+export const institutionIdAtom = atom<string | null>(null)
