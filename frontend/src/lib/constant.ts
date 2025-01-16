ef7b638ad506c14610c6b365663258c301c03660
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2025-01-16 22:13:04 +0530

    Update domain configuration to use environment variable for local IP

--- frontend/src/lib/constant.ts
+++ frontend/src/lib/constant.ts
@@ -1,2 +1,2 @@
-export const domain = "http://192.168.0.158:3000"
+export const domain = `http://${import.meta.env.VITE_LOCAL_IP}:3000`
 export const fastapidomain = `http://${import.meta.env.VITE_FASTAPI_HOST}:8000`
